dcd001f31df2d8ab6b27a22c9405dac19056150c
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-04-22 16:33:34 -0700

    feat: initial version

---
+++ script.js
@@ -0,0 +1,11 @@
+// @ts-check
+
+/**
+ * Merge a bunch of pull requests based on parameters
+ *
+ * @param {import('@octoherd/cli').Octokit} octokit
+ * @param {import('@octoherd/cli').Repository} repository
+ * @param {object} options
+ * @param {string} [options.author] GitHub username to filter the pull requests by
+ */
+export async function script(octokit, repository, { author }) {}
